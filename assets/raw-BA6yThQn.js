dc03b4774c68359add66045f9a72bacf979ce53d
John Hoffer <john@hoff.in>
2025-11-10 19:33:10 +0000

    update

--- assets/raw-BA6yThQn.js
+++
@@ -1,9 +0,0 @@
-import { B as BaseDecoder } from "./index-CQ22F1LN.js";
-class RawDecoder extends BaseDecoder {
-  decodeBlock(buffer) {
-    return buffer;
-  }
-}
-export {
-  RawDecoder as default
-};
